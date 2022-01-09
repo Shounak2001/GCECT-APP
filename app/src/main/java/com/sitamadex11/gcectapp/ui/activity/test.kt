50fcecf0fa295ce192b14b4d44c1304913a062ce
Anubhab Das <adas99201@gmail.com>
2022-01-09 18:48:53 +0530

    Contact_US, Syllabus, Collaboration

---
+++ app/src/main/java/com/sitamadex11/gcectapp/ui/activity/test.kt
@@ -0,0 +1,14 @@
+package com.sitamadex11.gcectapp.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sitamadex11.gcectapp.R
+
+class test : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.syllabus_it_page)
+
+
+    }
+}
